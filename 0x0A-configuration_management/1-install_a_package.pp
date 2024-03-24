e5a9c50ea50128a816ceae6f7d8824b442e68ca5
Ruyodan <salvator.hamdoune@gmail.com>
2024-03-24 07:18:52 -0700

    last check

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,11 @@
 # A puppet manifest installing flask v2.1.0 which is a package from pip3.
-# Specifying the version of flask to install using the install_options attribute
+# Specifying the version of flask to install using the install_options attribute.
 package { 'flask':
-  ensure          => '2.1.0',
-  provider        => 'pip3',
-  install_options => ['install', 'flask==2.1.0'],
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
